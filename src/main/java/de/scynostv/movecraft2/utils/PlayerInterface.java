8e2d91e551d2ac18ac8b3a31c39bba2dcd755fac
CrazyGamesMC <johndee3a@gmail.com>
2022-04-19 20:25:14 +0200

    Fixed version; Added vscode folders to gitignore

--- src/main/java/de/scynostv/movecraft2/utils/PlayerInterface.java
+++ src/main/java/de/scynostv/movecraft2/utils/PlayerInterface.java
@@ -1,39 +1,6 @@
 package de.scynostv.movecraft2.utils;
 
-import org.bukkit.entity.Player;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public class PlayerInterface {
-
-    private static Map playerMap = new HashMap<UUID, PlayerInterface>();
-
-    public static void addPlayer(Player player) {
-        playerMap.put(player.getUniqueId(), new PlayerInterface(player));
-        
-        
-        
-    }
-    
-    public static void deletePlayer(Player player) {
-        
-        var id = player.getUniqueId(); 
-        
-        if (!playerMap.containsKey(id)) return; 
-        
-        playerMap.remove(player.getUniqueId());
-    }
-
-
-
-    private Player playerReference;
-
-    public PlayerInterface(Player player) {
-        this.playerReference = player;
-    }
-
-
-
+  
 }
