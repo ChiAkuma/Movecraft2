2b234e5148014daef180286e7fdca2ff3d09fa43
CrazyGamesMC <johndee3a@gmail.com>
2022-04-19 21:23:03 +0200

    Create InteractEvent.java

---
+++ src/main/java/de/scynostv/movecraft2/events/InteractEvent.java
@@ -0,0 +1,13 @@
+package de.scynostv.movecraft2.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class InteractEvent {
+    
+    @EventHandler
+    public void coreBlockRightClick(PlayerInteractEvent e) {
+        
+    }
+
+}
