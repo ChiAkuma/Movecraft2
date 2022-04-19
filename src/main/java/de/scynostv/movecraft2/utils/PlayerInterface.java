0d5c7d968b2c9da02a0a537d42c1c6665b2b9795
CrazyGamesMC <johndee3a@gmail.com>
2022-04-19 20:53:08 +0200

    PlayerInterface done
    We still need to implement add and delete, when the player joins or leaves;

--- src/main/java/de/scynostv/movecraft2/utils/PlayerInterface.java
+++ src/main/java/de/scynostv/movecraft2/utils/PlayerInterface.java
@@ -1,6 +1,57 @@
 package de.scynostv.movecraft2.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
 
 public class PlayerInterface {
-  
+    private static Map playerMap = new HashMap<UUID, PlayerInterface>();
+
+    public static void add(Player player) {
+        playerMap.put(player.getUniqueId(), new PlayerInterface(player)); 
+    }
+
+    public static void delete(Player player) {
+        var id = player.getUniqueId(); 
+
+        if (!playerMap.containsKey(id)) return;
+
+        playerMap.remove(id);
+    }
+
+    public static PlayerInterface get(UUID id) {
+        return (PlayerInterface) playerMap.get(id);
+    }
+
+    public static PlayerInterface getByName(String name) {
+        for (var key : playerMap.keySet()) {
+            var pif = (PlayerInterface) playerMap.get(key);
+
+            if (pif.getPlayer().getName().equalsIgnoreCase(name)) return pif; 
+        }
+
+        return null; 
+    }
+
+    public static Player getByPlayer(Player player) {
+        var id = player.getUniqueId(); 
+
+        if (playerMap.containsKey(id)) 
+        return (Player) playerMap.get(id); 
+
+        return null; 
+    }
+    
+    
+    private Player playerReference; 
+    public PlayerInterface(Player player) {
+        this.playerReference = player; 
+    }
+
+    public Player getPlayer() {
+        return this.playerReference; 
+    }
+
 }
